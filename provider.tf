66aef41c14ca69993d86123159d3578bda8ac744
Sami Islam <islam.samipkr@gmail.com>
2023-09-14 20:44:30 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform{
+  required_providers{
+    azurerm={
+      source="hashicorp/azurerm"
+      version= ">=3.70.0"#this version is for azurerm, NOT terraform version
+    }
+  }
+  required_version="1.4.0"#this version is for Terraform Version, NOT azurerm
+}
+
+provider "azurerm"{
+  features{}
+  skip_provider_registration="true"
+}
