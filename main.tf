56debff265f24a4574b2e1d8f5c177788de9dabc
Sami Islam <islam.samipkr@gmail.com>
2023-09-15 19:38:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group""batcha06"{
     name="batch_A06_Resource_Group"
     location="Canada Central"   
 }
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
