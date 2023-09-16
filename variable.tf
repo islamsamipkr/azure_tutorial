c50b2cd473ee2f49269378616e7594d22f2a4f4c
Sami Islam <islam.samipkr@gmail.com>
2023-09-16 19:19:46 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,3 +10,11 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+variable "storage_account_name"{
+  type=string
+  default="batchA06"
+}
+variable "prefix"{
+  type=string
+  default="batchA06"
+}
