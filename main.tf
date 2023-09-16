0b90358de174806a969bd1951b155f1512ad5638
Sami Islam <islam.samipkr@gmail.com>
2023-09-16 19:28:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ resource "azurerm_resource_group""batcha06"{
     name="${var.prefix}_Resource_Group_${var.env}"
     location="Canada Central"   
 }
+
 resource "azurerm_storage_account" "awp" {
   name                     = "${var.prefix}-storageaccountname-${var.env}"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.batcha06.name
+  location                 = azurerm_resource_group.batcha06.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
