8539c70d76c449b34ceba94d231053bd9a3e643a
Sami Islam <islam.samipkr@gmail.com>
2023-09-21 22:41:50 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,19 @@
 # Rename these outputs in one of the files (main.tf or kubernetes.tf) to make them unique
 output "client_certificate_main" {
-  value     = azurerm_kubernetes_cluster.k8scluster[*].kube_config.0.client_certificate
+  value     = [for cluster in azurerm_kubernetes_cluster.k8scluster: cluster.client_certificate]
+#azurerm_kubernetes_cluster.k8scluster[*].kube_config.0.client_certificate
   sensitive = true
 }
 
-output "kube_config_main" {
-  value     = azurerm_kubernetes_cluster.k8scluster[*].kube_config_raw.0
-  sensitive = true
-}
+#output "kube_config_main" {
+#  value     = azurerm_kubernetes_cluster.k8scluster[*].kube_config_raw.0
+#  sensitive = true
+#}
+
+#output "client_certificate" {
+#  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+#}
+
+#output "kube_config" {
+#  value = azurerm_kubernetes_cluster.example.kube_config_raw
+#}
