24b24472173ad442af61dcc3d504291aa3438db7
Sami Islam <islam.samipkr@gmail.com>
2023-09-28 21:15:00 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "azureresourcegroup" {
   location = "Canada Central"
 }
 resource "azurerm_kubernetes_cluster" "batchabcd" {
-  name                = "batchabcd"
+  name                = local.cluster_names
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   dns_prefix          = "exampleaks1"
