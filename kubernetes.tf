bda456b00070ea9d55aa73045330b998694616cb
Sami Islam <islam.samipkr@gmail.com>
2023-09-28 23:19:52 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -38,3 +38,9 @@ output "kube_config" {
 
   sensitive = true
 }
+output "kube_id"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
+}
+output "kube_name"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
+}
