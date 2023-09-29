4a979f118c3f73cb2f9cde4f86d7de3d43a74df7
Sami Islam <islam.samipkr@gmail.com>
2023-09-28 23:13:05 -0400

    Update kubernetesnodepool.tf

--- kubernetesnodepool.tf
+++ kubernetesnodepool.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_cluster_node_pool" "kube1nodepool" {
  for_each               = azurerm_kubernetes_cluster.batchabcd
- name                   = azurerm_kubernetes_cluster.batchabcd.name
- kubernetes_cluster_id  = azurerm_kubernetes_cluster.batchabcd.id
+ name                   = each.key
+ kubernetes_cluster_id  = each.key
  vm_size                = "Standard_DS2_v2"
  node_count             = 1
 
