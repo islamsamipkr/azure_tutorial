0b20daf56fd4d5df6a6c1ca8823e277cceeafba0
Sami Islam <islam.samipkr@gmail.com>
2023-09-28 20:52:30 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,1 @@
-output "client_certificate_main" {
-  value     = [for cluster in azurerm_kubernetes_cluster.k8scluster: cluster.kube_config.0.client_certificate]
-  sensitive = true
-}
 
-output "kube_config_main" {
-  value     = [for cluster in azurerm_kubernetes_cluster.k8scluster: cluster.kube_config_raw]
-  sensitive = true
-}
