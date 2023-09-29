87d16b7f06a1a97612286634d275f63e8b2a2808
Sami Islam <islam.samipkr@gmail.com>
2023-09-28 21:59:30 -0400

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "azureresourcegroup" {
   location = "Canada Central"
 }
 resource "azurerm_kubernetes_cluster" "batchabcd" {
-  for_each            = {for cluster in local.cluster_names: cluster}
+  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
   name                = "${var.prefix}cluster"
   location            = azurerm_resource_group.azureresourcegroup.location
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
