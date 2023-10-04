53fbab287128374d888b781fae6da5d278d1c93c
Sami Islam <islam.samipkr@gmail.com>
2023-10-03 22:52:46 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "azurerm_service_plan" "batcha06sp" {
 
 resource "azurerm_linux_web_app" "batcha06webapp" {
   for_each            = azurerm_service_plan.batcha06sp
-  name                = "{$each.key}"
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   location            = azurerm_resource_group.azureresourcegroup.location
   service_plan_id     = each.value.id
