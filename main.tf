6d90609ed744bc502c95cdb55e869a975aae8f6f
Sami Islam <islam.samipkr@gmail.com>
2023-10-03 22:42:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,8 +13,28 @@ locals{
 ])
 }
 
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "batcha06" {
 for_each={for rg in local.linux_app_list:"${rg.name}" => rg}  
-name     = each.value.name
-  location = each.value.location
+  name      = each.value.name
+  location  = each.value.location
 }
+
+resource "azurerm_service_plan" "batcha06sp" {
+  for_each            ={for sp in local.linux_app_list: "$sp.name"=>sp }
+  name                = "${each.key}"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "batcha06webapp" {
+  for_each            = azurerm_service_plan.batcha06sp
+  name                = "{$each.key}"
+  resource_group_name = azurerm_resource_group.batcha06.name
+  location            = azurerm_service_plan.batcha06sp.location
+  service_plan_id     = each.value.id
+
+  site_config {}
+}
+
