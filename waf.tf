83a5fd8cbca6217b13e93d6f89a7b6bd603438ef
Tomas Saez <tomas.saezbin@gmail.com>
2023-10-11 05:26:32 -0300

    fix(Terraform): possible fix to waf rules

--- waf.tf
+++ waf.tf
@@ -4,8 +4,43 @@ resource "azurerm_web_application_firewall_policy" "wafpol" {
   resource_group_name = azurerm_resource_group.azureresourcegroup.name
   location            = azurerm_resource_group.azureresourcegroup.location
 
-  custom_rules =each.value.custom_rules
+  dynamic "custom_rules" {
+    for_each = each.value.custom_rules
+    content {
+      name      = custom_rules.name
+      priority  = custom_rules.priority
+      rule_type = custom_rules.rule_type
+      action = custom_rules.action
 
-  managed_rules=each.value.managed_rules
+      dynamic "match_conditions" {
+        for_each = custom_rules.match_conditions
+        content {
+          operator            = match_conditions.operator
+          negation_condition  = match_conditions.negation_condition
+          match_values        = match_conditions.match_values
+          dynamic "match_variables" {
+            for_each = match_conditions.match_variables
+            content {
+              variable_name = match_variables.variable_name
+            }
+          }
+        }
+        
+      }
+    }
   }
 
+  dynamic managed_rules {
+    for_each = each.value.managed_rules
+    content {
+      dynamic managed_rule_set {
+        for_each = managed_rules.managed_rule_set
+        content {
+          type      = managed_rule_set.type
+          version   = managed_rule_set.version
+        }
+      }
+
+    }
+  } 
+}
