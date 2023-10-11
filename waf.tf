a389881998b64c16cb2ac3cb00b28e95e30fdd04
Sami Islam <islam.samipkr@gmail.com>
2023-10-10 22:38:49 -0400

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -5,27 +5,27 @@ resource "azurerm_web_application_firewall_policy" "wafpol" {
   location            = azurerm_resource_group.azureresourcegroup.location
 
   custom_rules {
-    name      = each.value.name
-    priority  = each.value.priority 
-    rule_type = each.value.rule_type
+    name      = each.value.custom_rules.name
+    priority  = each.value.custom_rules.priority 
+    rule_type = each.value.custom_rules.rule_type
 
     match_conditions {
       match_variables {
-        variable_name = each.value.variable_name
+        variable_name = each.value.custom_rules.variable_name
       }
 
-      operator           = each.value.operator
-      negation_condition = each.value.negation_condition
-      match_values       = each.value.match_values
+      operator           = each.value.custom_rules.operator
+      negation_condition = each.value.custom_rules.negation_condition
+      match_values       = each.value.custom_rules.match_values
     }
-    action = each.value.action
+    action = each.value.custom_rules.action
 
   }
   managed_rules {
 
     managed_rule_set {
-      type    = each.value.type
-      version = each.value.version
+      type    = each.value.managed_rules.type
+      version = each.value.managed_rules.version
     }
   }
 }
