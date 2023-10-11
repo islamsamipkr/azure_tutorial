941332b910fbe7d9625bde7dafae95c87e28f46f
Sami Islam <islam.samipkr@gmail.com>
2023-10-10 22:21:02 -0400

    Update waf.tf

--- waf.tf
+++ waf.tf
@@ -24,8 +24,8 @@ resource "azurerm_web_application_firewall_policy" "wafpol" {
   managed_rules {
 
     managed_rule_set {
-      type    = "OWASP"
-      version = "3.2"
+      type    = each.value.type
+      version = each.value.version
     }
   }
 }
